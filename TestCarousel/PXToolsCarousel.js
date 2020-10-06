219ec20e828e6867ae80ee393b7e4a9cf27580fe
Marcelo Giazzon <mgiazzon@puntoexe.com.uy>
2020-10-06 00:55:19 -0300

    Add files via upload

---
+++ TestCarousel/PXToolsCarousel.js
@@ -0,0 +1,11 @@
+(function(){
+	console.log("teste");
+
+	$(document).ready(function(){
+		console.log('Antes PXToolsDraw.reder();');
+		PXToolsOwlCarousel();
+		console.log('Depois PXToolsDraw.reder();');
+	  	//$('.owl-carousel').owlCarousel();
+	});
+
+})()
